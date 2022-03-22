5dbea9ceb004847946b31d58254cd1be3d598035
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2022-03-22 19:32:52 +0600

    added lab final

---
+++ Lab final/RPS/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RPS
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+            StreamReader sr = new StreamReader("SWE4201MarkSheet.txt");
+            string line;
+            while((line = sr.ReadLine()) != null)
+            {
+                string[] col = new string[10];
+                col = line.Split(',');
+                Student dummy = new Student(col[0], col[1], col[2], col[3], col[4], col[5], col[6], col[7], col[8], col[9]);
+                students.Add(dummy);
+            }
+
+            for (int i = 1; i < students.Count; i++)
+            {
+                listBoxStudents.Items.Add(students[i].name + "\t" + students[i].id);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+            var stdnt = from studentss in students
+                        where studentss.id == id
+                        select studentss;
+
+            foreach(var s in stdnt)
+            {
+                textBox6.Text = s.attendence;
+                textBox2.Text = s.quiz[0];
+                textBox3.Text = s.quiz[1];
+                textBox4.Text = s.quiz[2];
+                textBox5.Text = s.quiz[3];
+                textBox7.Text = s.CalculateQuizMark().ToString();
+                textBox8.Text = s.mid;
+                textBox9.Text = s.final;
+                textBox10.Text = s.viva;
+                textBox11.Text = s.CalculateTotal().ToString();
+                textBox12.Text = s.CalculatePercentage().ToString();
+                textBox13.Text = s.CalculateGrade();
+            }
+        }
+    }
+}
---
+++ Lab final/RPS/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPS
+{
+    internal class Student
+    {
+        public string id, name;
+        public string attendence, mid, final, viva;
+        public string[] quiz = new string[4];
+        public double percentage;
+        public int total1;
+
+        public Student(string id, string name, string attendence, string quiz1, string quiz2, string quiz3, string quiz4, string mid, string final, string viva)
+        {
+            this.id = id;
+            this.name = name;
+            this.attendence = attendence;
+            quiz[0] = quiz1;
+            quiz[1] = quiz2;
+            quiz[2] = quiz3;
+            quiz[3] = quiz4;
+            this.mid = mid;
+            this.final = final;
+            this.viva = viva;
+        }
+        public int CalculateQuizMark()
+        {
+            Array.Sort(quiz);
+            int total = int.Parse(quiz[1]) + int.Parse(quiz[2]) + int.Parse(quiz[3]);
+            return total;
+        }
+        public int CalculateTotal()
+        {
+            total1 = int.Parse(mid) + int.Parse(final) + int.Parse(viva) + CalculateQuizMark();
+            return total1;
+        }
+
+        public double CalculatePercentage()
+        {
+            percentage = (double)CalculateTotal() / 300;
+            return percentage * 100;
+        }
+        public string CalculateGrade()
+        {
+            if (percentage >= 80f && percentage <= 100f)
+                return "A+";
+            if (percentage >= 75f && percentage <= 79f)
+                return "A";
+            if (percentage >= 70f && percentage <= 74f)
+                return "A-";
+            if (percentage >= 65f && percentage <= 69f)
+                return "B+";
+            if (percentage >= 60f && percentage <= 64f)
+                return "B";
+            if (percentage >= 55f && percentage <= 59f)
+                return "B-";
+            if (percentage >= 50f && percentage <= 54f)
+                return "C+";
+            if (percentage >= 45f && percentage <= 49f)
+                return "C";
+            if (percentage >= 40f && percentage <= 44f)
+                return "D";
+            else
+                return "F";
+        }
+    }
+}
